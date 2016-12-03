f0704568384087331578c7e9c55d77c3f8d823f0
Maximilian Michels <mxm@apache.org>
2016-12-03 23:53:41 +0100

    restructure app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
-from bottle import Bottle, run
+from bottle import Bottle, run, static_file, template
 
 app = Bottle()
 
+static_root = "./static"
+
+@app.route('/query/<name>/<start>/<end>/<date>/<time>/<app>')
+def request(**params):
+    return template("request_template.html", params)
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    return static_file("index.html", root=static_root)
+
+@app.route('/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root=static_root)
 
 if __name__ == '__main__':
     run(app, host="0.0.0.0", port=19999)
